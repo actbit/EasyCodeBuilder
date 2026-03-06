db2192d93914e2f55dcc55d21fca47872d729732
actbit <57023457+actbit@users.noreply.github.com>
2026-03-06 18:30:54 +0900

    [fix] proj

--- EasyCodeBuilder/Program.cs
+++ EasyCodeBuilder/Program.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using EasyCodeBuilder.Application.Interfaces;
+using EasyCodeBuilder.Core.Interfaces;
+using EasyCodeBuilder.Infrastructure.Services;
+using EasyCodeBuilder.Presentation.Forms;
 
 namespace EasyCodeBuilder
 {
     static class Program
     {
+        /// <summary>
+        /// サービスプロバイダー（DIコンテナ）
+        /// </summary>
+        internal static IServiceProvider ServiceProvider { get; private set; }
+
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
@@ -16,7 +23,45 @@ namespace EasyCodeBuilder
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            // DIコンテナの設定
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+
+            // MainFormをDIコンテナから取得して起動
+            Application.Run(ServiceProvider.GetRequiredService<MainForm>());
+        }
+
+        /// <summary>
+        /// DIコンテナにサービスを登録します
+        /// </summary>
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // Core Services
+            services.AddSingleton<IValidationService, ValidationService>();
+            services.AddSingleton<IVariableService, VariableService>();
+            services.AddSingleton<ITypeService, TypeService>();
+
+            // Application Services
+            services.AddSingleton<IMessageBoxService, MessageBoxService>();
+            services.AddSingleton<ICompilerService, CompilerService>();
+            services.AddSingleton<IFileDialogService, FileDialogService>();
+            services.AddSingleton<IProjectPersistenceService, ProjectPersistenceService>();
+            services.AddSingleton<IStatementFactory, StatementFactory>();
+
+            // Forms
+            services.AddTransient<MainForm>();
+            services.AddTransient<AddControlDialog>();
+        }
+
+        /// <summary>
+        /// サービスロケーターからサービスを取得します
+        /// デザイナー互換性のために使用
+        /// </summary>
+        public static T GetService<T>()
+        {
+            return ServiceProvider.GetRequiredService<T>();
         }
     }
 }
